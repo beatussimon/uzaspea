cd429457a902367548de2b059e3837df2b9b3bc8
bea <beatussimon47@gmail.com>
2026-06-18 19:02:47 +0300

    Use SSH_ASKPASS in push_script.sh

--- push_script.sh
+++ push_script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 ssh-agent -s > /tmp/agent.env
 source /tmp/agent.env
-chmod +x /home/bea/uzaspea/add_key.exp
-/home/bea/uzaspea/add_key.exp
+export SSH_ASKPASS=/home/bea/uzaspea/askpass.sh
+export DISPLAY=:0
+chmod +x /home/bea/uzaspea/askpass.sh
+setsid ssh-add /home/bea/.ssh/id_ed25519 < /dev/null
 cd /home/bea/uzaspea
 git remote set-url origin git@github.com:beatussimon/uzaspea.git
 git push origin --force --all
 git push origin --force --tags
+
